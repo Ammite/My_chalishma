eff52c5e79406fe4489e8b6f544786d5f2aaa19b
Ammite <baa21v@gmail.com>
2015-08-23 20:40:07 +0600

    Улучшил свой калькулятор теперь он может прибовлять, отнимать, умножать и делить!

--- Calc.cpp
+++ Calc.cpp
@@ -1,7 +1,25 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int a,b;
-	cin>>a>>b;
-	cout<<"a+b="<<a+b<<endl;	
+	int a,b,x;
+	cout<<"Write one number from 1 to 4 if you write:"<<endl<<"1)a+b"<<endl<<"2)a-b"<<endl<<"3)a*b"<<endl<<"4)a/b"<<endl;
+	cin>>x;
+	cout<<"Okay, please enter two numbers a and b:";
+	if(x==1){
+		cin>>a>>b;
+		cout<<a<<"+"<<b<<"="<<a+b<<endl;
+	}
+	if(x==2){
+		cin>>a>>b;
+		cout<<a<<"-"<<b<<"="<<a-b<<endl;
+	}	
+	if(x==3){
+		cin>>a>>b;
+		cout<<a<<"*"<<b<<"="<<a*b<<endl;
+	}
+	if(x==4){
+		cin>>a>>b;
+		cout<<a<<"/"<<b<<"="<<a/b<<endl;
+	}
+	return 0;
 }
